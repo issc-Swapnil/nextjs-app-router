4e7d7d1847fe90576a3d53eab76c106ff8291d2e
Naeema bargir <bargirn5@gmail.com>
2024-03-15 15:48:31 +0530

    UI fixed

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 const Header = () => {
-
     const [isActive, setIsActive] = useState<boolean>(false);
     const [collapse, setCollapse] = useState<boolean>(false)
+
     // event listener
     useEffect(() => {
         const handleScroll = () => {
@@ -26,14 +26,15 @@ const Header = () => {
     return (
         <>
             <header>
-                <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+                <nav className={`border-gray-200 px-4 lg:px-6 py-2.5 fixed w-full z-10 transition-all
+                 ${isActive ? "bg-white py-4 shadow-md" : "bg-none py-6 text-white"} `}>
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                         <Link href="/" className="flex items-center">
                             <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Logo" />
                             <span className="self-center text-xl font-semibold whitespace-nowrap ">Flight</span>
                         </Link>
                         <div className="flex items-center lg:order-2">
-                            <Link href="/login" className="text-gray-800  hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Log in</Link>
+                            <Link href="/login" className="hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Log in</Link>
                             <Link href="#" className="text-white bg-[#0000ff] focus:ring-4 focus:ring-[#0000ff] font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Get started</Link>
                             <button data-collapse-toggle="mobile-menu-2" type="button"
                                 onClick={handleCollapse}
@@ -42,29 +43,29 @@ const Header = () => {
                                 aria-controls="mobile-menu-2"
                                 aria-expanded="false">
                                 <span className="sr-only">Open main menu</span>
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                                <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                                <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </button>
                         </div>
-                        <div className={`${!collapse ? 'hidden' : 'block'} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} id="mobile-menu-2">
+                        <div className={`${!collapse ? 'hidden' : 'block bg-white text-black'} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} id="mobile-menu-2">
                             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                                 <li>
-                                    <Link href="#" className="block py-2 pr-4 pl-3 t text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0" aria-current="page">Home</Link>
+                                    <Link href="#" className="block py-2 pr-4 pl-3 t  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0" aria-current="page">Home</Link>
                                 </li>
                                 <li>
-                                    <Link href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Company</Link>
+                                    <Link href="#" className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Company</Link>
                                 </li>
                                 <li>
-                                    <Link href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Marketplace</Link>
+                                    <Link href="#" className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Marketplace</Link>
                                 </li>
                                 <li>
-                                    <Link href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Features</Link>
+                                    <Link href="#" className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Features</Link>
                                 </li>
                                 <li>
-                                    <Link href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Team</Link>
+                                    <Link href="#" className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Team</Link>
                                 </li>
                                 <li>
-                                    <Link href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Contact</Link>
+                                    <Link href="#" className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-[#0000ff] lg:p-0">Contact</Link>
                                 </li>
                             </ul>
                         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
     <main>
       {/* banner */}
       <div className=" bg-[#05203c] px-4 lg:px-6 py-24">
-        <div className="mx-auto max-w-screen-xl">
+        <div className="mx-auto max-w-screen-xl mt-12">
           <h1 className='text-white mb-4 text-[2rem] font-[700]'>Millions of cheap prices. One simple search.</h1>
           <SearchBox />
         </div>
@@ -57,7 +57,7 @@ export default function Home() {
         <div className="mx-auto max-w-screen-xl">
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 ">
             {
-              [0, 1, 2].map(() => (
+              [0, 1, 2, 3,4,5].map(() => (
                 <PlaceCard />
               ))
             }
